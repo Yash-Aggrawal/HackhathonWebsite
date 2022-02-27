4046247c51f15ab8ba50433410ab2ff1e29b2936
lemontreejs13 <secretstuff130@gmail.com>
2022-02-27 22:10:25 +0530

    sponsor-contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      'xs': '400px',
-    }
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 };
